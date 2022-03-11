39a527f7db77ed79d8ceb68363b0f80f9e3e9f63
Asus VivoBook <novacntikafitri@gmail.com>
2022-03-11 16:01:33 +0700

    Percobaan Literal

---
+++ src/main/kotlin/2_Opertor.kt
@@ -0,0 +1,13 @@
+fun main() {
+    //komentar 1 baris
+    /*
+    1 baris atau lebih
+     */
+    /*
+    dokumentari
+     */
+    var a = 7
+    var b = 6
+    var c = (a++ + b++) /2.0
+    println(c+b)
+}
