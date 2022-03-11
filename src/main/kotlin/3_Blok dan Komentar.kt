5e00863d1f3811205c79a0a868f5673c50992bae
Asus VivoBook <novacntikafitri@gmail.com>
2022-03-11 16:07:32 +0700

    Percobaan Literal

---
+++ src/main/kotlin/3_Blok dan Komentar.kt
@@ -0,0 +1,11 @@
+fun main() {
+    println("Hello, Saya Baru Belajar Kotlin Nich")
+    //Komentar 1 Baris
+    /*
+    ini contoh multi baris
+     */
+    //Komentar multi baris
+    /**
+     * contoh komentar multi baris
+     */
+}
