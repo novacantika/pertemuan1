95ae2275d55b236da1b7e7f795a3bf478c908f34
Asus VivoBook <novacntikafitri@gmail.com>
2022-03-11 14:28:20 +0700

    Percobaan Literal

---
+++ src/main/kotlin/1_Literal.kt
@@ -0,0 +1,14 @@
+fun main(args: Array<String>) {
+    var intLiteral = 5
+    var doubleLiteral = .02
+    var stringLiteral = "Hello"
+    var charLiteral = '1'
+    var boolLiteral = true
+    //tampilkan variabel menggunakan string template
+    println("intLiteral = $intLiteral")
+    println("doubleLiteral = $doubleLiteral")
+    println("stringLiteral = $stringLiteral")
+    println("charLiteral  = $charLiteral")
+    println("boolLiteral = $boolLiteral")
+
+}
