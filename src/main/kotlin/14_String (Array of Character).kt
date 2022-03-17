cb213573458d55d6b7ea565840d427a7d1fa5c71
Asus VivoBook <novacntikafitri@gmail.com>
2022-03-17 19:36:52 +0700

    percobaan tipe data

--- src/main/kotlin/14_String (Array of Character).kt
+++ src/main/kotlin/14_String (Array of Character).kt
@@ -1,10 +1,35 @@
+// Penggunaan tipe data String
+//fun main() {
+////    val myName: String = "Depandi Enda"
+////    for(chr in myName){
+////        print(chr)
+////    }
+////    print('\n')
+
+//menggabungkan String dengan tipe data lain dengan menggunakan operator +,
+// sehingga tipe data yang digabung otomatis akan melebur kedalam String.
+//            fun main(args: Array<String>) {
+//                val s = "abc" + 1
+//                println(s + "def")
+//            }
+
+// Untuk dapat menyimpan data String yang memuat keywords Kotlin bisa dengan menggunakan
+//raw String yang dipisahkan dengan triple quotes (""").
+//        fun  main(args: Array<String>) {
+//            var text = """
+//            for (c in "foo")
+//                print(c)
+//        """
+//            println(text)
+//        }
+
+//Kita juga dapat menghapus spasi yang tidak diperlukan dalam raw String
 fun main(args: Array<String>) {
-    //String dapat diakses dengan operasi indeks array contoh s[i] atau dengan menggunakan for-loop
-    val myName: String = "Nova Cantika Fitri"
-    for(chr in myName){
-        print(chr) }
-    print('\n')
-    //menggabungkan String dengan tipe data lain dengan menggunakan operator +,
-    val s = "abc" + 1
-    println(s + "def")
+    var text = """
+            |Tell me and I forget.
+            |Teach me and I remember.
+            |Involve me and I learn.
+            |(Benjamin Franklin)
+            """.trimMargin()
+    println(text)
 }
